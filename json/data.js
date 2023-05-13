f6d806700ffdd2364ac2cbb243b446cd30503b4c
banyaadski <89918249+banyaadski@users.noreply.github.com>
2023-05-13 15:18:06 +0300

    Add files via upload
    Json

---
+++ json/data.js
@@ -0,0 +1,20 @@
+const dataInfo = [
+    {
+        "count": 1094417,
+        "gender": "male",
+        "name": "peter",
+        "probability": 1.0
+      },
+      {
+        "count": 425630,
+        "gender": "male",
+        "name": "ivan",
+        "probability": 1.0
+      },
+      {
+        "count": 6510,
+        "gender": "female",
+        "name": "ivy",
+        "probability": 0.96
+      }
+    ];
